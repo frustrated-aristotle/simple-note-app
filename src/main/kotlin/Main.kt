f456ab6439345723ad5a591e10b9951008165f28
frustrated-aristotle <85283219+frustrated-aristotle@users.noreply.github.com>
2023-06-01 13:12:36 +0300

    Reading Notepad name Writing the note to that notepad.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,25 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val notepad = hashMapOf<String, ArrayList<String>>()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var firstNote = ArrayList<String>()
+    firstNote.add("Take A Note")
+    notepad.put("NotePad1", firstNote)
+
+    println("Welcome! Your notepads are: " + notepad.keys)
+
+    println("Please write the name of the notepad you want")
+    var notepadName = readLine()
+    var strNotePadName : String = notepadName.toString()
+
+    println("It is time to write your note!")
+
+
+
+    var notes = ArrayList<String>();
+    var note = readLine()
+    notes.add(note.toString())
+    notepad.put(strNotePadName, notes)
+
+    println("There is your note from ${notepadName}")
+    println(notepad[notepadName])
 }
